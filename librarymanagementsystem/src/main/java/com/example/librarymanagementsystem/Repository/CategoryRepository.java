3b107e573e3b4e6f44a1f569368893f75f6eeeb2
TaoTaoNgo <vuminhao1998@gmail.com>
2021-11-04 10:50:15 +0700

    Create Repository Category

---
+++ librarymanagementsystem/src/main/java/com/example/librarymanagementsystem/Repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.librarymanagementsystem.Repository;
+
+import com.example.librarymanagementsystem.Model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
