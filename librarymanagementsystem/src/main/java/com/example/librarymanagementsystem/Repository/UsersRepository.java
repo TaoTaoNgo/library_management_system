7105fb3e790d6997215e74700cfa04d5254948bf
Trieu210 <43270896+Trieu210@users.noreply.github.com>
2021-11-17 10:11:29 +0700

    Updated Users Repo

--- librarymanagementsystem/src/main/java/com/example/librarymanagementsystem/Repository/UsersRepository.java
+++ librarymanagementsystem/src/main/java/com/example/librarymanagementsystem/Repository/UsersRepository.java
@@ -2,9 +2,16 @@ package com.example.librarymanagementsystem.Repository;
 
 import com.example.librarymanagementsystem.Model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 
 public interface UsersRepository extends JpaRepository<Users,Long> {
+    public List<Users> findAllByOrderByDisplayNameAsc();
+    public List<Users> findAllByActiveOrderByDisplayNameAsc(Integer active);
+    public Users findByUsername(String username);
+
 }
